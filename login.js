b534256a503ffa86c1ec8ffd6b54b7a8f3ccec63
오성준 <dhflgjsxj2@gmail.com>
2024-05-17 13:05:36 +0900

    feat: add disabled button function in login.js

--- login.js
+++ login.js
@@ -1,7 +1,18 @@
 const email = document.querySelector('.input-email');
 const password = document.querySelector('input.input-password');
 const label = document.querySelector('.label-password');
+const authBtn = document.querySelector('.auth-btn');
 const pattern = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
+
+function validate() {
+  if (
+    email.value &&
+    pattern.test(email.value) &&
+    password.value &&
+    password.value.length >= 8
+  )
+    return true;
+}
 
 function showError(errMsg, errElement) {
   errElement.classList.add('error-outline');
@@ -18,13 +29,27 @@ function cleanError(errElement) {
   }
 }
 
+function btnDisabled() {
+  authBtn.classList.add('auth-btn--disabled');
+}
+
+function btnActive() {
+  authBtn.classList.remove('auth-btn--disabled');
+}
+
 function checkEmail() {
   const errMsg1 = '이메일을 입력해주세요';
   const errMsg2 = '잘못된 이메일 형식입니다';
   if (!email.value) {
     showError(errMsg1, email);
+    btnDisabled();
   } else if (!pattern.test(email.value)) {
     showError(errMsg2, email);
+    btnDisabled();
+  }
+
+  if (validate()) {
+    btnActive();
   }
 }
 
@@ -33,8 +58,14 @@ function checkPassword() {
   const errMsg2 = '비밀번호를 8자 이상 입력해주세요';
   if (!password.value) {
     showError(errMsg1, password);
+    btnDisabled();
   } else if (password.value.length < 8) {
     showError(errMsg2, password);
+    btnDisabled();
+  }
+
+  if (validate()) {
+    btnActive();
   }
 }
 
